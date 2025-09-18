78461dcb01c0c0416d156dc9d9028b21809e8fb0
anasnaeemai <wckdaiagency@gmail.com>
2025-09-18 17:28:47 +0400

    Update setup_ctf.sh

--- setup_ctf.sh
+++ setup_ctf.sh
@@ -1,19 +1,10 @@
-# As root, create the challenge
-   mkdir -p /home/challenge/documents/secret
-   mkdir -p /home/challenge/public
-   
-   # Create decoy files
-   echo "This is a public document" > /home/challenge/public/readme.txt
-   echo "Meeting notes from last week" > 
-/home/challenge/documents/notes.txt
-   
-   # Create hidden file with flag
-   echo "FLAG{you_found_the_secret_file!}" > 
-/home/challenge/documents/secret/.hidden_flag
-   
-   # Create another hidden file as red herring
-   echo "This is just a hidden config file" > 
-/home/challenge/documents/.config
-   
-   # Set permissions
-   chmod -R 755 /home/challenge
+#!/bin/bash
+echo "Setting up CTF Challenge..."
+mkdir -p /home/challenge/documents/secret
+mkdir -p /home/challenge/public
+echo "This is a public document" > /home/challenge/public/readme.txt
+echo "Meeting notes from last week" > /home/challenge/documents/notes.txt
+echo "FLAG{you_found_the_secret_file!}" > /home/challenge/documents/secret/.hidden_flag
+echo "This is just a hidden config file" > /home/challenge/documents/.config
+chmod -R 755 /home/challenge
+echo "CTF Challenge setup complete!"
